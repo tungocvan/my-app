35b1d8ee06795b2dbc1238b872cca2ba0436f81b
tungocvan <tungocvan@gmail.com>
2020-10-18 06:04:12 +0700

    v5

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -10,12 +10,12 @@ class AdminController {
      let img = files.tmp_name;
      //let data = img.replace(/^data:image\/\w+;base64,/, "");
      //let buf = new Buffer(data, 'base64');
-     let data = {'name':'tu ngoc van'};
-      fs.writeFile(files.name, data, function(err) {
-          if (err) throw err;
-          res.json({"message":"Upload Ok !!!"});
-      });   
-      //res.json({"message":"Upload Ok !!!"});
+      let data = {'name':'tu ngoc van'};
+      // fs.writeFile(files.name, data, function(err) {
+      //     if (err) throw err;
+      //     res.json({"message":"Upload Ok !!!"});
+      // });   
+      res.json({"message":data});
   } 
 
 }
