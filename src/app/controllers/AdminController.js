9358a7d6aef8d92904ca91d803dabcb777e0a546
tungocvan <tungocvan@gmail.com>
2020-10-18 13:19:39 +0700

    v17

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,21 +1,31 @@
 const fs = require('fs');
 const Buffer = require('buffer/').Buffer;
+const path = require('path'); 
 class AdminController {
   // [GET] /
   index(req, res) {
     res.render('admin',{ layout : 'layoutAdmin'});
   } 
-  upload(req, res) {
+  upload(req, res,next) {
      let files  = req.body;
      let name = files.name;
      let t = global.basedir + '/'+name;
      let img = files.tmp_name;
      let data = img.replace(/^data:image\/\w+;base64,/, "");
      let buf = new Buffer(data, 'base64');     
-      fs.writeFile(t, buf, (err) => {
-          if (err) throw err;
-          res.json({"message":"Upload Ok !!!"});
+     fs.mkdir(path.join(__dirname, 'test'), (err) => { 
+      if (err) { 
+          return console.error(err); 
+      } 
+      console.log('Directory created successfully!'); 
       }); 
+    //  if (!fs.existsSync(des)) {
+    //         fs.mkdirSync(des, 0744);        
+    //   }
+      // fs.writeFile(t, buf, (err) => {
+      //     if (err) throw err;
+      //     res.json({"message":"Upload Ok !!!"});        
+      // }); 
       // let writeStream = fs.createWriteStream(t); 
       // writeStream.write(data, 'base64');
       // writeStream.on('finish', () => {
@@ -24,6 +34,7 @@ class AdminController {
       // writeStream.end();
       //res.json({"message":t});
   } 
+  
 }
 
 module.exports = new AdminController();
