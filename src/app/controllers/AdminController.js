46998aa8d7af9f78a455b271373ac94afc216ac2
tungocvan <tungocvan@gmail.com>
2020-10-18 14:30:40 +0700

    tnv2

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -9,30 +9,15 @@ class AdminController {
   upload(req, res,next) {
      let files  = req.body;
      let name = files.name;
-     let t = global.basedir + '/'+name;
+     let t = global.basedir + '/publics/img/'+name;
      let img = files.tmp_name;
      let data = img.replace(/^data:image\/\w+;base64,/, "");
-     let buf = new Buffer(data, 'base64');     
-    //  fs.mkdir(path.join(__dirname, 'test'), (err) => { 
-    //   if (err) { 
-    //       return console.error(err); 
-    //   } 
-    //   console.log('Directory created successfully!'); 
-    //   }); 
-    //  if (!fs.existsSync(des)) {
-    //         fs.mkdirSync(des, 0744);        
-    //   }
-    fs.writeFile(t, buf, (err) => {
-          if (err) throw err;
-          res.json({"message":"Upload Ok !!!"});        
-    }); 
-      // let writeStream = fs.createWriteStream(t); 
-      // writeStream.write(data, 'base64');
-      // writeStream.on('finish', () => {
-      //    console.log('wrote all data to file');
-      // });
-      // writeStream.end();
-    //  res.json({"message":t});
+     let buf = new Buffer(data, 'base64');  
+     fs.writeFile(t, buf, (err) => {
+            if (err) throw err;
+            res.json({"message":"Upload Ok !!!"});        
+     }); 
+   
   } 
   
 }
